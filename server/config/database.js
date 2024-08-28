ccc16a57da86c0947b2b13d03408549353130917
DevarshiPatel63 <devarshi.learn63@gmail.com>
2024-08-28 12:22:15 +0530

    express.js server started

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false'
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false';
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI , ()=>{
-        console.log("Connected to Mongo Successfully")
-    })
-}
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to Mongo Successfully");
+    } catch (error) {
+        console.error("Failed to connect to Mongo", error);
+    }
+};
 
 module.exports = connectToMongo;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 const connectToMongo = require('./config/database')
+const express =require('express')
 
-connectToMongo()
+connectToMongo();
+
+const app = express()
+const port = 8000
+
+app.get('/',(req , res)=>{
+    res.send('hello')
+})
+
+app.listen(port, ()=> {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
