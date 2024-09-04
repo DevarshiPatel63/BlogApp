32deda357dc7da7cda1dd7f7526121b4a412f160
DevarshiPatel63 <devarshi.learn63@gmail.com>
2024-09-04 17:26:48 +0530

    added Blog model

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,28 +1,38 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const User = require('./User'); 
 
-const BlogSchema = new mongoose.Schema({
+const Blog = sequelize.define('blog', {
   title: {
-    type: String,
-    required: [true, 'Please provide a title for the blog post'],
+    type: DataTypes.STRING,
+    allowNull: false,
     trim: true,
-    maxlength: [100, 'Title cannot be more than 100 characters']
+    validate: {
+      len: [3, 100],   
+    }
   },
   content: {
-    type: String,
-    required: [true, 'Please provide content for the blog post'],
+    type: DataTypes.TEXT,
+    allowNull: false,
     trim: true,
-    minlength: [10, 'Content must be at least 10 characters long']
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    validate: {
+      len: [10, 10000], 
+    }
   },
   category: {
-    type: String,
-    required: [true, 'Please specify a category for the blog post'],
-    enum: ['Technology', 'Lifestyle', 'Food', 'Travel', 'Other']
+    type: DataTypes.ENUM('Technology', 'Lifestyle', 'Food', 'Travel', 'Other'),
+    allowNull: false
   },
+  authorId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User, 
+      key: 'id'
+    },
+    allowNull: false
+  }
+}, {
+  timestamps: true
 });
 
-module.exports = mongoose.model('Blog', BlogSchema);
+module.exports = Blog;
